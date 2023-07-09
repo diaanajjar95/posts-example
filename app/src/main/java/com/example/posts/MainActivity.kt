652feee790a924e028d6133f26fa99a24045046d
DiaaNajjar <diaa.najjar.ro@gmail.com>
2023-07-09 03:40:26 +0300

    navigation component added

--- app/src/main/java/com/example/posts/MainActivity.kt
+++ app/src/main/java/com/example/posts/MainActivity.kt
@@ -3,6 +3,15 @@ package com.example.posts
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * beneficial links :
+ *
+ * Navigation Component Guide : https://developer.android.com/guide/navigation/get-started
+ * GitHub add Your Android Project Guide : https://medium.com/@alessandro.lombardi.089/how-to-push-an-android-studio-project-to-github-via-command-line-20fca1262beb
+ *
+ *
+ * */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
